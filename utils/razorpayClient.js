08397fb4ef8d7071a7cd6a1658b7d1b861af5f89
indalnovacompany-oss <indalnovacompany@gmail.com>
2025-09-18 00:50:00 +0530

    Update razorpayClient.js

--- utils/razorpayClient.js
+++ utils/razorpayClient.js
@@ -1,8 +1,13 @@
 import Razorpay from "razorpay";
-import dotenv from "dotenv";
-dotenv.config();
+
+const RAZORPAY_KEY_ID = process.env.RAZORPAY_KEY_ID;
+const RAZORPAY_KEY_SECRET = process.env.RAZORPAY_KEY_SECRET;
+
+if (!RAZORPAY_KEY_ID || !RAZORPAY_KEY_SECRET) {
+  throw new Error("Razorpay environment variables are missing!");
+}
 
 export const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET
+  key_id: RAZORPAY_KEY_ID,
+  key_secret: RAZORPAY_KEY_SECRET,
 });
